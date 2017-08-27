0eff698e038ebfcc509d7473eac4fd232dfac2ef
FelSiq <felipe.siqueira@usp.br>
2017-08-27 13:55:23 -0300

    New code: data-type=microarray dataset='adrenal-dahia' alg=random-forest noise-detection=FALSE

---
+++ microarray-algorithm-random-forest-dataset-adrenal-dahia.R
@@ -0,0 +1,50 @@
+# Specifications: ------------------------------------------
+# 	Algorithm : 'Random Forest Classfier' from library 'randomForest'
+# 	Noise Filtering (1 or 0): 0
+# 	Additional Libraries : 'caTools' (for data split)
+# 	Dataset type: Microarray
+# 	Dataset deeper specifications: Bruno Ferez de Souza thesis @ 
+#		http://www.teses.usp.br/teses/disponiveis/55/55134/tde-04012011-142551/pt-br.php
+# 	Dataset name: 'dataset_adrenal_dahia.txt'
+# ----------------------------------------------------------
+
+# 1) Get the Dataset.
+dataset <- read.csv('dataset_adrenal_dahia.txt', sep = ' ')
+
+# 2) Load all the necessary packages for this test
+library(caTools) # For data splitting
+library(randomForest) # For prediction test
+
+# 3) Transpose the dataset (?)
+dataset.t <- as.data.frame(t(dataset))
+
+# 3.a) Correct the column names
+# The output/target feature will be named 'Class', and the prediction 
+# features will be enumered with a natural number sequence.
+colnames(dataset.t) <- c('Class', seq(1, ncol(dataset.t) - 1))
+
+# Set the target feature to R factor type
+dataset.t$Class <- factor(dataset.t$Class, levels = unique(dataset.t$Class))
+
+# 4) Split the dataset in Train and Test sets
+datasplit <- sample.split(dataset.t$'1', SplitRatio = 0.75)
+set.train <- subset(dataset.t, datasplit)
+set.test <- subset(dataset.t, !datasplit)
+
+# 5) Train a randomForest classifier (from randomForest package)
+# A custom seed will be used to ensure experiment replication
+set.seed(101010)
+classifier <- randomForest(
+	formula = Class ~ .,
+	ntree = 100,
+	x = set.train[-1],
+	y = set.train$Class,
+	importance = FALSE)
+
+# 6) Use the classifier to predict some results
+predictions <- predict(
+	object = classifier,
+	newdata = set.test)
+
+# 7) Check the confusion matrix
+table(set.test$Class, predictions)
