1cdcaa3b124d64be5771e45868c5f605a1f9f0ff
FelSiq <felipe.siqueira@usp.br>
2017-12-22 20:18:48 -0200

    Attached a simple K-fold Cross Validation on main code.

--- main.R
+++ main.R
@@ -34,55 +34,61 @@ for (datasetID in 1:nrow(config.DATASET_SEQ)) {
 	majorityLabel <- ifelse(sum(dataset$Class == labels[1]) >= length(dataset$Class)/2, 1, 2)
 	dataset$Class <- factor(ifelse(dataset$Class == labels[majorityLabel], 0, 1))
 
-	# 3) Split the dataset in Train and Test sets
-	datasplit <- sample.split(dataset$Class, SplitRatio = config.DATASPLIT_RATE)
-	set.train <- subset(dataset, datasplit)
-	set.test <- subset(dataset, !datasplit)
+	# 
+	bags <- sample(1:config.FOLDS_NUM_CROSS_VALIDATION, size = nrow(dataset), replace = TRUE)
 
-	for (smoteEnabled in config.SMOTE_SEQ) {
-		# If smote enable, then call ubSMOTE. Otherwise, the 'smotedTrainSet' is 'secretly' just the set.train
-		if (smoteEnabled) {
-			aux <- ubSMOTE(dataset[-which(colnames(dataset) == 'Class')], dataset$Class)
-			smotedTrainSet <- data.frame(aux$X)
-			smotedTrainSet$Class <- aux$Y
-			# @ Garbage collection (to avoid memory issues) -------
-			rm(aux)
-			gc()
-			# @ ---------------------------------------------------
-		} else {
-			smotedTrainSet <- set.train
-		}
-		
-		# 5) Input artificial noise
-		smotedTrainSet.noise <- rand(smotedTrainSet, config.ERROR_INPUT_RATE)
-		
-		# For each classifier
-		for (classifierID in config.CLASSIFIER_SEQ) {
-			# 4) Train a classifier with the original data (before artificial noise inputation)
-			predictionsOriginal <- general.fitAndPredict(smotedTrainSet, set.test, classifierID)
-			accOriginal <- caret::confusionMatrix(predictionsOriginal, set.test$Class)$overall[1]
-			
-			# 6) Train a randomForest classifier (from randomForest package) with class noise
-			predictionsNoise <- general.fitAndPredict(smotedTrainSet.noise$data, set.test, classifierID)
-			accNoise <- caret::confusionMatrix(predictionsNoise, set.test$Class)$overall[1]
-			
-			# For each noise filter...
-			for (noiseFilterID in config.NOISEFILTER_SEQ) {
-				# 7) Use a noise filter here
-				filterResult <- general.callNoiseFilter(smotedTrainSet.noise$data, noiseFilterID)
+	# 
+	accuracy <- vector()
+	for (i in 1:config.FOLDS_NUM_CROSS_VALIDATION) {
+		# 
+		set.train <- subset(dataset, i != bags)
+		set.test <- subset(dataset, i == bags)
 
-				# 8) Train a new classifier, after the noise filtering 
-				predictionsFiltered <- general.fitAndPredict(filterResult$cleanData, set.test, classifierID)
-				accFiltered <- caret::confusionMatrix(predictionsFiltered, set.test$Class)$overall[1]
-
+		for (smoteEnabled in config.SMOTE_SEQ) {
+			# If smote enable, then call ubSMOTE. Otherwise, the 'smotedTrainSet' is 'secretly' just the set.train
+			if (smoteEnabled) {
+				aux <- ubSMOTE(dataset[-which(colnames(dataset) == 'Class')], dataset$Class)
+				smotedTrainSet <- data.frame(aux$X)
+				smotedTrainSet$Class <- aux$Y
 				# @ Garbage collection (to avoid memory issues) -------
-				rm(filterResult)
+				rm(aux)
 				gc()
 				# @ ---------------------------------------------------
+			} else {
+				smotedTrainSet <- set.train
+			}
+			
+			# 5) Input artificial noise
+			smotedTrainSet.noise <- rand(smotedTrainSet, config.ERROR_INPUT_RATE)
+			
+			# For each classifier
+			for (classifierID in config.CLASSIFIER_SEQ) {
+				# 4) Train a classifier with the original data (before artificial noise inputation)
+				predictionsOriginal <- general.fitAndPredict(smotedTrainSet, set.test, classifierID)
+				accOriginal <- caret::confusionMatrix(predictionsOriginal, set.test$Class)$overall[1]
+				
+				# 6) Train a randomForest classifier (from randomForest package) with class noise
+				predictionsNoise <- general.fitAndPredict(smotedTrainSet.noise$data, set.test, classifierID)
+				accNoise <- caret::confusionMatrix(predictionsNoise, set.test$Class)$overall[1]
+				
+				# For each noise filter...
+				for (noiseFilterID in config.NOISEFILTER_SEQ) {
+					# 7) Use a noise filter here
+					filterResult <- general.callNoiseFilter(smotedTrainSet.noise$data, noiseFilterID)
 
-				# 11) Check accuracy results
-				cat(config.DATASET_SEQ[datasetID, 1], classifierID, noiseFilterID, 
-					smoteEnabled, accOriginal, accNoise, accFiltered, '\n', sep='|')
+					# 8) Train a new classifier, after the noise filtering 
+					predictionsFiltered <- general.fitAndPredict(filterResult$cleanData, set.test, classifierID)
+					accFiltered <- caret::confusionMatrix(predictionsFiltered, set.test$Class)$overall[1]
+
+					# @ Garbage collection (to avoid memory issues) -------
+					rm(filterResult)
+					gc()
+					# @ ---------------------------------------------------
+
+					# 11) Check accuracy results
+					cat(i, config.DATASET_SEQ[datasetID, 1], classifierID, noiseFilterID, 
+						smoteEnabled, accOriginal, accNoise, accFiltered, '\n', sep='|')
+				}
 			}
 		}
 	}
--- src/config.R
+++ src/config.R
@@ -25,11 +25,11 @@ library(unbalanced) # For SMOTE (treatment of unbalanced dataset)
 library(caret) # For a good confusion matrix
 
 config.DATASET_SEQ <- t(matrix(c(
-		c('BRCA.mirnaseq.txt', 'Micro-RNA'),
-		c('KICH.mirnaseq.txt', 'Micro-RNA'),
-		c('THCA.mirnaseq.txt', 'Micro-RNA'),
 		c('CHOL.mirnaseq.txt', 'Micro-RNA'),
+		c('KICH.mirnaseq.txt', 'Micro-RNA'),
 		c('LUAD.mirnaseq.txt', 'Micro-RNA'),
+		c('THCA.mirnaseq.txt', 'Micro-RNA'),
+		c('BRCA.mirnaseq.txt', 'Micro-RNA'),
 		c('dataset_lymphoma_shipp.txt', 'Microarray'),
 		c('dataset_adrenal_dahia.txt', 'Microarray'),
 		c('dataset_mixed_chowdary.txt', 'Microarray'),
@@ -37,7 +37,7 @@ config.DATASET_SEQ <- t(matrix(c(
 		c('dataset_prostate_singh.txt', 'Microarray')
 	), 2, 10))
 config.CLASSIFIER_SEQ <- c('KNN', 'SVM', 'RF')
-config.NOISEFILTER_SEQ <- c('HARF', 'AENN', 'INFFC')
+config.NOISEFILTER_SEQ <- c('HARF', 'AENN')
 
 DEBUG = TRUE 
 if (!DEBUG) {
