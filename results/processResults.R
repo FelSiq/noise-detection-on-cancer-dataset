051d2db2230469ad50fc1736b1ce3536acfb7268
FelSiq <felipe.siqueira@usp.br>
2018-02-20 15:05:43 -0300

    Improved and Added extra result visualization.

--- results/processResults.R
+++ results/processResults.R
@@ -11,8 +11,9 @@ metadataPathList <- list.files(pattern = '.*\\.dat', recursive = TRUE)
 
 printOriginalAcc <- FALSE
 printFilterAcc <- FALSE
-printPValues <- FALSE
-plotDensity <- TRUE
+printPValues <- TRUE
+plotDensity <- FALSE
+plotNoiseCompare <- FALSE
 
 # The order of the configuration below follows strictly the pattern on my report and does matter.
 config.SMOTE_SEQ <- c(FALSE, TRUE)
@@ -146,11 +147,11 @@ if (printFilterAcc) {
 # http://blog.minitab.com/blog/adventures-in-statistics-2/how-to-correctly-interpret-p-values
 if (printPValues) {
 	library(metap)
-	sink(file = 'processedPValues.out', append = TRUE)
+	# sink(file = 'processedPValues.out', append = TRUE)
 	charAsciiIndex <- 65
 	for (r in metadataPathList) {
 		metadata <- getMetadata(r)
-		cat('\\colcell\\dados', intToUtf8(charAsciiIndex), 'Nome & ',sep='')
+		# cat('\\colcell\\dados', intToUtf8(charAsciiIndex), 'Nome & ',sep='')
 		charAsciiIndex <- charAsciiIndex + 1
 		for (s in config.SMOTE_SEQ) {
 			for (f in config.NOISEFILTER_SEQ) {
@@ -158,14 +159,19 @@ if (printPValues) {
 				if (nrow(curMetadata) > 0) {
 					SumLogPValue <- sumlog(curMetadata$PVFiltered)$p
 					numlen <- signifPlaces(SumLogPValue)
+					if (SumLogPValue > 0.05) {
+						cat(r, s, f, '\n\t', curMetadata$PVFiltered, '\nResult:')
+						print(sumlog(curMetadata$PVFiltered))
+						cat('\n')
+					}
 
 					color <- if (SumLogPValue <= 0.05) 'Blue' else 'Red'
-					cat('\\cellcolor{', color ,'} ', sep='')
+					# cat('\\cellcolor{', color ,'} ', sep='')
 
-					cat(if (SumLogPValue > 1e-10) removeZero(round(SumLogPValue, numlen)) else 0.0, sep='')
+					# cat(if (SumLogPValue > 1e-10) removeZero(round(SumLogPValue, numlen)) else 0.0, sep='')
 
-					if (!(f == 'ENG' && s == TRUE))
-						cat(' & ')				
+					# if (!(f == 'ENG' && s == TRUE))
+						# cat(' & ')				
 				}
 			}
 		}
@@ -177,6 +183,8 @@ if (plotDensity) {
 	alldata <- list()
 	alldata$NSMOTE <- NULL
 	alldata$YSMOTE <- NULL
+	png('AccuracyGeneralAnalysis.png', width=720, height=720)
+	layout(matrix(c(1,2,3,3), 2, 2, byrow = TRUE))
 	for (s in config.SMOTE_SEQ) {
 		accdata <- list()
 		accdata$HARF <- NULL
@@ -198,21 +206,19 @@ if (plotDensity) {
 			}
 		}
 
-		png(paste('densityPlot_SMOTE_', s, '.png', sep=''))
 		i <- 1
 		for (aux in accdata) {
 			if (i == 1) {
-				plot(density(aux), type='l', col = i, main=paste('Densidade de acurácia', (if(s) 'com SMOTE' else '')), 
+				plot(density(aux, to=1.0), type='l', col = i, main=paste('Densidade de acurácia', (if(s) 'com SMOTE' else 'sem SMOTE')), 
 					ylim=c(0.0, 7.0), ylab='Densidade', xlab='N = 225')
 			} else {
-				lines(density(aux), type='l', col = i)
+				lines(density(aux, to=1.0), type='l', col = i, lty=i)
 			}
-			text(x = 0.5, y = 6.5 - 0.25 * i, labels = config.NOISEFILTER_SEQ[i],
+			text(x = 0.5, y = 6.5 - 0.5 * i, labels = config.NOISEFILTER_SEQ[i],
 				col = 1,pos=4,family='Bookman Old Style')
-			rect(0.465, 6.469 - 0.25 * i, 0.4995, 6.55 - 0.25 * i, col=i,border=NA)
+			rect(0.465, 6.469 - 0.5 * i, 0.4995, 6.55 - 0.5 * i, col=i,border=NA)
 			i <- i + 1
 		}
-		dev.off()
 
 		if (s == TRUE) {
 			alldata$YSMOTE <- c(accdata$HARF, accdata$AENN, accdata$INFFC, accdata$ENG)
@@ -220,18 +226,37 @@ if (plotDensity) {
 			alldata$NSMOTE <- c(accdata$HARF, accdata$AENN, accdata$INFFC, accdata$ENG)
 		}
 	}
-	png('SMOTEAnalysis.png')
-	plot(density(alldata$NSMOTE),col='red', main='Impacto do SMOTE', ylab='Densidade', xlab='N = 900')
-	lines(density(alldata$YSMOTE), col='blue')
+	plot(density(alldata$NSMOTE, to=1.0),col='red', main='Impacto do SMOTE', ylab='Densidade', xlab='N = 900', lty=2)
+	lines(density(alldata$YSMOTE, to=1.0), col='blue')
 	text(x = 0.35, y = 5.25, labels = 'sem SMOTE',
 				col = 'black',pos=4,family='Bookman Old Style')
-	text(x = 0.35, y = 5.5, labels = 'com SMOTE',
+	text(x = 0.35, y = 5.75, labels = 'com SMOTE',
 				col = 'black',pos=4,family='Bookman Old Style')
-	rect(0.305, 5.4635, 0.358, 5.545, col='blue', border=NA)
-	rect(0.305, 5.23, 0.358, 5.31, col='red', border=NA)
+	rect(0.305, 5.175, 0.35, 5.31, col='red', border=NA)
+	rect(0.305, 5.7, 0.35, 5.845, col='blue', border=NA)
 	dev.off()
 }
 
+
+if (plotNoiseCompare) {
+	png(paste('FilterComparison.png'), width=720, height=720)
+	par(mfrow=c(2,2))
+	for (f in config.NOISEFILTER_SEQ) {
+		accFiltered <- vector()
+		accCorrupted <- vector()
+		for (d in metadataPathList) {
+		metadata <- getMetadata(r)
+			accFiltered <- c(accFiltered, metadata[metadata$Filter == f, 'predFiltered'])
+			accCorrupted <- c(accCorrupted, metadata[metadata$Filter == f, 'predCorrupted'])
+		}
+
+		plot(density(accFiltered, to=1.0), type='l', col = 'firebrick2', 
+			main=paste('Densidade de acurácia (filtro: ', f, ')'), ylab='Densidade', xlab='N = 450')
+		lines(density(accCorrupted, to=1.0), type='l', col = 'navy', lty=2)
+
+	}
+	dev.off()
+}
 
 sink(NULL)
 # ---------------------------------------
